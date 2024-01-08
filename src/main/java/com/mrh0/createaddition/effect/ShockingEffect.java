31dc02e1833a25c1a04db0c246f9c26e6c7dffc5
blue <midnight@nightmarish.blue>
2024-01-08 15:13:59 +0000

    The shocking effect causes entities to spasm uncontrollably.

--- src/main/java/com/mrh0/createaddition/effect/ShockingEffect.java
+++ src/main/java/com/mrh0/createaddition/effect/ShockingEffect.java
@@ -1,15 +1,46 @@
 package com.mrh0.createaddition.effect;
 
-import com.mrh0.createaddition.CreateAddition;
-
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Supplier;
 
 
 public class ShockingEffect extends MobEffect {
 	public ShockingEffect() {
 		super(MobEffectCategory.HARMFUL, 15453236);
 		//setRegistryName(new ResourceLocation(CreateAddition.MODID, "shocking"));
+	}
+
+	@Override
+	public void applyEffectTick(@NotNull LivingEntity entity, int amplifier) {
+		jostleEntity(entity, amplifier);
+	}
+
+	public void jostleEntity(LivingEntity entity, int amplifier) {
+		float maxMove = 0.2f * (amplifier + 1);
+		RandomSource random = entity.level.random;
+		Supplier<Float> randFloat = () -> (random.nextFloat() - 0.5f);
+
+		entity.setYBodyRot(entity.yBodyRot + randFloat.get() * maxMove * 25);
+		entity.setXRot(entity.getXRot() + randFloat.get() * maxMove * 25);
+		entity.setYRot(entity.getYRot() + randFloat.get() * maxMove * 25);
+		entity.setYHeadRot(entity.yHeadRot + randFloat.get() * maxMove * 25);
+
+		Vec3 delta = entity.getDeltaMovement();
+		Vec3 randVec = new Vec3(randFloat.get() * maxMove, 0f, randFloat.get() * maxMove);
+
+		if (entity.isOnGround() && random.nextInt(3) == 0)
+			randVec = randVec.add(0f, randFloat.get() * maxMove * 5, 0f);
+		entity.setDeltaMovement(delta.add(randVec));
+	}
+
+	@Override
+	public boolean isDurationEffectTick(int duration, int amplifier) {
+		return true;
 	}
 }
